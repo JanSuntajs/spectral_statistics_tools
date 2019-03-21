061dbc78855b68540b3cef9fa1279e61784d47ac
Jan Suntajs <Jan.Suntajs@ijs.si>
2019-03-21 10:28:06 +0100

    Working on setup.py file and installation; changed folder structure

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+
+setup(name='spectral_statistics_tools',
+      version='1.0.0',
+      description='The module for spectral statistics calculations.',
+      url='https://github.com/JanSuntajs/spectral_statistics_tools',
+      author='Jan Suntajs',
+      author_email='Jan.Suntajs@ijs.si',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
