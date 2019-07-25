b772d6ebedb752ad1298f32ba2c25020beb1737e
Jan Suntajs <jansuntajs@gmail.com>
2019-07-25 11:23:51 +0200

    working on the setup.py script, removing json

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(name='spectral_statistics_tools',
       license='MIT',
       packages=find_packages(),
       install_requires=['numpy', 'scipy',
-                        'numba', 'future', 'h5py', 'json'],
+                        'numba', 'future', 'h5py'],
       zip_safe=False)
