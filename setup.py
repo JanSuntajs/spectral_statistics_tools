e239881c165083af08190868897fbdf41eafa838
Jan Suntajs <jansuntajs@gmail.com>
2021-07-21 10:17:34 +0200

    Removing install requires lines

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup, find_packages
-
+# make sure numpy, numba, scipy and future are preexisting in the
+# environent in which this is installed
 
 setup(name='spectral_statistics_tools',
-      version='1.1.0',
+      version='1.1.1',
       description='The module for spectral statistics calculations.',
       url='https://github.com/JanSuntajs/spectral_statistics_tools',
       author='Jan Suntajs',
       author_email='Jan.Suntajs@ijs.si',
       license='MIT',
       packages=find_packages(),
-      install_requires=['numpy', 'scipy',
-                        'numba', 'future'],
+      install_requires=[ ],  # 'numpy', 'scipy',
+                             #'numba', 'future'],
       zip_safe=False)
